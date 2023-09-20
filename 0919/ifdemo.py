d2ac2b54f70bd1e9596f5ddcdae2df4358208641
ninaaano <tvxqinak@gmail.com>
2023-09-20 16:02:01 +0900

    add ifdemo.py

---
+++ 0919/ifdemo.py
@@ -0,0 +1,13 @@
+# avg = float(input("Enter avg : "))
+# if 90 <= avg <= 100 :
+#     print("A")
+# elif 80 <= avg <= 90 : print("B")
+# elif 70 <= avg <= 80 : print("C")
+# elif 60 <= avg <= 70 : print("D")
+# else : print("F")
+
+season = input("season? ").lower()
+if season == "spring" : print("개나리")
+elif season == "summer" : print("수박")
+elif season == "fall" : print("전어")
+else : print("방어")
